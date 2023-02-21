c7e6e0355fc6e4b34815d30923710eee38ad02dd
juliakocik <julia.kocik02@gmail.com>
2023-02-21 21:39:05 +0100

    Add Navigation component

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navigation.scss'
+
+const Navigation = (): JSX.Element => {
+  return (
+    <div className='navigation_container'>Navigation</div>
+  )
+}
+
+export default Navigation
