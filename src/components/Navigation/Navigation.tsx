06baff1406d48d3094aa9e4247c7708149396ca1
juliakocik <julia.kocik02@gmail.com>
2023-02-21 21:52:19 +0100

    Add Navigation feature

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,7 +3,25 @@ import './Navigation.scss'
 
 const Navigation = (): JSX.Element => {
   return (
-    <div className='navigation_container'>Navigation</div>
+    <div className='navigation_container'>
+      <ul>
+        <li>
+          <a href="#start">Start</a>
+        </li>
+        <li>
+          <a href="#uslugi">Oferta</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <li>
+          <a href="#opinie">Opinie</a>
+        </li>
+        <li>
+          <a href="#kontakt">Kontakt</a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
