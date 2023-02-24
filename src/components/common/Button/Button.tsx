8a15c3f7277165936c8d0484aa932ddab53484bc
juliakocik <julia.kocik02@gmail.com>
2023-02-24 20:15:05 +0100

    Add Button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ title, color }): JSX.Element => {
   const className = `btn ${color === 'light' ? 'btn_light' : ''} ${color === 'dark' ? 'btn_dark' : ''}`
   return (
         <button className={className}>
-            <span>{title}</span>
+            <span><b>{title}</b></span>
         </button>
   )
 }
