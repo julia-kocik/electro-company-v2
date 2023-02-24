859e32b36e251d51470c72be4a6bdb2296012d18
juliakocik <julia.kocik02@gmail.com>
2023-02-24 22:11:03 +0100

    Refine Offer component

--- src/components/sections/Offer/Offer.tsx
+++ src/components/sections/Offer/Offer.tsx
@@ -3,7 +3,18 @@ import './Offer.scss'
 
 const Offer = (): JSX.Element => {
   return (
-    <div className='offer_container' id='offer'>Offer</div>
+    <div className='offer_container' id='offer'>
+      <img src="https://cdn.pixabay.com/photo/2019/01/08/10/58/smart-home-3920905_960_720.jpg" alt="Smart Home" />
+      <div className="offer_content">
+        <ul>
+          <li>Instalacje elektryczne</li>
+          <li>Inteligentne domy</li>
+          <li>Systemy alarmowe</li>
+          <li>Systemy monitoringu</li>
+          <li>Instalacje fotowoltaiczne</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
