03a1c191b4b829d642015b3eea118d2a675a1902
juliakocik <julia.kocik02@gmail.com>
2023-02-25 16:25:01 +0100

    Add styling adjustments

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../../common/Button/Button'
 import NavBar from '../../common/NavBar/NavBar'
+import Title from '../../common/Title/Title'
 import './Header.scss'
 
 const Header = (): JSX.Element => {
@@ -9,7 +10,7 @@ const Header = (): JSX.Element => {
       <NavBar />
       <div className='header_content'>
         <div className='header_content_box'>
-          <h1>Elektryczne rozwiązania na miarę Twoich potrzeb</h1>
+          <Title title='Elektryczne rozwiązania na miarę Twoich potrzeb'/>
           <h3>Nasza firma oferuje szeroki zakres usług elektrycznych, obejmujący zarówno instalacje, jak i naprawy i konserwację. Zaufaj naszej wiedzy i doświadczeniu, a zyskasz spokojną głowę w kwestii bezpieczeństwa i niezawodności swoich instalacji elektrycznych.</h3>
           <a href="#offer">
             <Button title='Oferta' color='light'/>
