a3167c76d1d8d754be6410d2771ac6813fa587a6
juliakocik <julia.kocik02@gmail.com>
2023-02-25 14:41:40 +0100

    Add Contact form component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ title, color }): JSX.Element => {
   const className = `btn ${color === 'light' ? 'btn_light' : ''} ${color === 'dark' ? 'btn_dark' : ''}`
   return (
-        <button className={className}>
+        <button type='submit' className={className}>
             <span><b>{title}</b></span>
         </button>
   )
--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import Button from '../../common/Button/Button'
 import Title from '../../common/Title/Title'
 import './Contact.scss'
 
 const Contact = (): JSX.Element => {
   return (
     <div className='contact_container' id='contact'>
-      <Title title='Skontaktuj się z nami'/>
-      <form>
-        <input type="text" id="name" name="name"/><br />
-        <input type="email" id="email" name="email"/><br/>
-        <textarea id="message" name="message"></textarea>
-      </form>
+      <Title title='Skontaktuj się z nami' />
+      <div className="form_container">
+        <form>
+          <input type="text" id="name" name="name" placeholder='Imię'/><br />
+          <input type="email" id="email" name="email" placeholder='Adres email'/><br/>
+          <textarea id="message" name="message" placeholder='Wiadomość'></textarea>
+          <Button title='Wyślij' color='light'/>
+        </form>
+      </div>
     </div>
   )
 }
