69d4a44e8ea648bcd10b64c96c18c49ef0e954cd
juliakocik <julia.kocik02@gmail.com>
2023-02-25 12:45:28 +0100

    Continue working on Contact component

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Title from '../../common/Title/Title'
 import './Contact.scss'
 
 const Contact = (): JSX.Element => {
   return (
-    <div className='contact_container' id='contact'>Contact</div>
+    <div className='contact_container' id='contact'>
+      <Title title='Skontaktuj się z nami'/>
+      <form>
+        <input type="text" id="name" name="name"/><br />
+        <input type="email" id="email" name="email"/><br/>
+        <textarea id="message" name="message"></textarea>
+      </form>
+    </div>
   )
 }
 
