ce746b801010ae77e7eca2c9e3304fb928dbcfd0
juliakocik <julia.kocik02@gmail.com>
2023-02-25 12:45:11 +0100

    Adjust BlogSection component

--- src/components/sections/BlogSection/BlogSection.tsx
+++ src/components/sections/BlogSection/BlogSection.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import Button from '../../common/Button/Button'
+import Title from '../../common/Title/Title'
 import './BlogSection.scss'
 
 const BlogSection = (): JSX.Element => {
   const str: string = 'Smart homes are equipped with advanced technology that allows homeowners to remotely control and automate various functions of their homes, such as lighting, temperature, and security systems. With smart homes, homeowners can use their smartphones or other internet-connected devices to control their homes systems from anywhere in the world'
   return (
     <div className='blog_container' id='blogsection'>
-      <article className='blog_article'>
-        <h2>Title of Article</h2>
-        <p>{str.substring(0, 200)}...</p>
-        <Button title='czytaj wiecej' color='light'/>
-      </article>
-      <article className='blog_article'>
-        <h2>Title of Article</h2>
-        <p>{str.substring(0, 200)}...</p>
-        <Button title='czytaj wiecej' color='light'/>
-      </article>
-      <article className='blog_article'>
-        <h2>Title of Article</h2>
-        <p>{str.substring(0, 200)}...</p>
-        <Button title='czytaj więcej' color='light'/>
-      </article>
+      <Title title='Poczytaj naszego bloga'/>
+      <div className="blog_container_inner">
+        <article className='blog_article'>
+          <h2>Title of Article</h2>
+          <p>{str.substring(0, 200)}...</p>
+          <Button title='czytaj wiecej' color='light'/>
+        </article>
+        <article className='blog_article'>
+          <h2>Title of Article</h2>
+          <p>{str.substring(0, 200)}...</p>
+          <Button title='czytaj wiecej' color='light'/>
+        </article>
+        <article className='blog_article'>
+          <h2>Title of Article</h2>
+          <p>{str.substring(0, 200)}...</p>
+          <Button title='czytaj więcej' color='light'/>
+        </article>
+      </div>
     </div>
   )
 }
