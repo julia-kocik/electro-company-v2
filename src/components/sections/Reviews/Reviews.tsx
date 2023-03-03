0b229cf7fc07c402365bf66a95617de36a5da4c2
juliakocik <julia.kocik02@gmail.com>
2023-03-03 20:10:07 +0100

    Continue working on reviews container

--- src/components/sections/Reviews/Reviews.tsx
+++ src/components/sections/Reviews/Reviews.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Title from '../../common/Title/Title'
 import './Reviews.scss'
+import { BsCircle } from 'react-icons/bs'
 
 const arr = ['super', 'extra', 'wow', 'awesome']
 
 const Reviews = (): JSX.Element => {
   const [active, setActive] = useState(0)
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setActive(active => {
@@ -14,21 +16,30 @@ const Reviews = (): JSX.Element => {
         }
         return active + 1
       })
-    }, 3000)
+    }, 5000)
     return () => {
       clearInterval(intervalId)
     }
   }, [])
+
+  const onDotClickHandler = (dot: number): void => {
+    setActive(dot)
+  }
   return (
-        <div className="reviews_container" id='reviews'>
+        <div className='reviews_container' id='reviews'>
             <Title title='Opinie klientów'/>
             <div className='reviews_container_inner'>
               {arr.map((item, index) => (
                 <div key={index} className={`reviews_content ${active === index ? 'reviews_content_active' : ''}`}>
-                  { item}
+                  {item}
                 </div>
               ))}
-            </div>
+      </div>
+      <div className='reviews_dots_container'>
+      {Array(arr.length).fill(<BsCircle className='reviews_icon'/>).map((item, index) => (
+        <span onClick={() => onDotClickHandler(index)} key={index}>{item}</span>
+      ))}
+      </div>
         </div >
   )
 }
