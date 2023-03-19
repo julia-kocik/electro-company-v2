f0322af6a418f3f12d033775ec2445fdff048702
juliakocik <julia.kocik02@gmail.com>
2023-03-19 18:19:43 +0100

    Adjust article to use firebase

--- src/components/views/Article/Article.tsx
+++ src/components/views/Article/Article.tsx
@@ -1,37 +1,84 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getArticles } from '../../../config'
 import NavBar from '../../common/NavBar/NavBar'
 import Title from '../../common/Title/Title'
 import './Article.scss'
+import { useParams } from 'react-router-dom'
+import { secondsToDate } from '../../../utils/utils'
+// import { secondsToDate } from '../../../utils/utils'
 
+interface ArticleType {
+  title: string
+  content: string
+  main_image: string
+  published: {
+    seconds: number
+  }
+}
+
+const initialState = {
+  title: '',
+  content: '',
+  main_image: '',
+  published: {
+    seconds: 0
+  }
+}
 const BlogContent = (): JSX.Element => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [article, setArticle] = useState<ArticleType | undefined>(initialState)
+  const params = useParams<{ id: string }>()
+
+  // use the id parameter here
+  const { id } = params
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
+  useEffect(() => {
+    const fetchArticles = async (): Promise<void> => {
+      try {
+        const data = await getArticles()
+        if (data && id) {
+          const findMatchingArticle = data.find((_article, index) => index === +id)
+          setArticle(findMatchingArticle)
+        }
+        setLoading(false)
+        setError(false)
+      } catch (error) {
+        console.error(error)
+        setError(true)
+      }
+    }
+
+    fetchArticles()
+  }, [])
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  const formattedDate = secondsToDate(article?.published.seconds).toLocaleDateString()
   return (
     <div className='blog_content_container'>
         <NavBar />
       <div className="blog_content_overlay">
-        <article className='article_container'>
-          <div className="img_container">
-            <img src="https://images.pexels.com/photos/225232/pexels-photo-225232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Smart Home" />
-          </div>
-          <div className='article_content'>
-            <Title title='Title of the article'></Title>
-            <div className='article_data'><span>Data publikacji: </span></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut tristique dolor. Aenean ut justo non nulla vestibulum molestie. Aliquam scelerisque purus vitae urna placerat lobortis. Nullam semper nisi vel libero facilisis consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque rutrum leo vel turpis bibendum, id congue ex imperdiet. Duis tincidunt sollicitudin dolor, vel hendrerit sem rutrum vel. Nulla facilisi. Fusce vel est at magna rhoncus consequat. Maecenas suscipit bibendum urna, quis dapibus ipsum rutrum nec. Vivamus eu nulla ac elit sagittis blandit. Nullam vel lorem non mauris tristique posuere eget in tortor. Duis ac nisi euismod, laoreet quam vel, bibendum orci. Aenean tincidunt justo id mi imperdiet, sed suscipit orci efficitur.
-
-  Sed laoreet eleifend justo, vel commodo augue dapibus non. Praesent vitae tortor massa. Pellentesque eu lectus at velit efficitur gravida ac eu justo. Sed id tellus eu tellus congue elementum. Sed vel justo eget magna eleifend consectetur. Nunc eget dui vitae tellus dictum bibendum eu eget sapien. Praesent nec eros fermentum, malesuada augue a, malesuada quam. Sed bibendum libero eget metus faucibus, nec molestie metus lobortis. Nulla in ultrices leo. Sed sodales turpis augue, eget molestie arcu malesuada ut. In faucibus pellentesque nibh vitae varius. Integer et tellus eu velit fermentum tincidunt. Sed nec dui enim. In eu dui a quam bibendum vehicula. Aenean semper enim quis elit vehicula, at consequat neque efficitur. Donec feugiat malesuada elit, sed aliquet elit.
-
-  Praesent in mauris quis sapien dictum pretium. Integer rutrum ultrices tincidunt. Mauris nec mauris quam. Etiam in arcu vitae quam rhoncus gravida at eu enim. Praesent varius, turpis eget consectetur viverra, nibh mi hendrerit velit, in faucibus nisl lacus at ante. Sed vestibulum laoreet arcu quis bibendum. Duis tincidunt, lacus in luctus dignissim, purus sapien sollicitudin tortor, id dignissim neque ex non lacus. Suspendisse lobortis hendrerit turpis at suscipit. Nam euismod, mi vel egestas pretium, justo quam lobortis nunc, eu bibendum purus eros at ipsum. Etiam eget massa id arcu laoreet rhoncus. Vestibulum efficitur bibendum diam, id iaculis ante rutrum eu. Aliquam erat volutpat. Nam suscipit consequat tellus, ut varius nisl blandit sit amet. Sed tempor turpis at magna bibendum, in pharetra metus lacinia. Curabitur ut turpis diam. Nulla sit amet est in ante consequat consequat vitae et arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut tristique dolor. Aenean ut justo non nulla vestibulum molestie. Aliquam scelerisque purus vitae urna placerat lobortis. Nullam semper nisi vel libero facilisis consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque rutrum leo vel turpis bibendum, id congue ex imperdiet. Duis tincidunt sollicitudin dolor, vel hendrerit sem rutrum vel. Nulla facilisi. Fusce vel est at magna rhoncus consequat. Maecenas suscipit bibendum urna, quis dapibus ipsum rutrum nec. Vivamus eu nulla ac elit sagittis blandit. Nullam vel lorem non mauris tristique posuere eget in tortor. Duis ac nisi euismod, laoreet quam vel, bibendum orci. Aenean tincidunt justo id mi imperdiet, sed suscipit orci efficitur.
-
-  Sed laoreet eleifend justo, vel commodo augue dapibus non. Praesent vitae tortor massa. Pellentesque eu lectus at velit efficitur gravida ac eu justo. Sed id tellus eu tellus congue elementum. Sed vel justo eget magna eleifend consectetur. Nunc eget dui vitae tellus dictum bibendum eu eget sapien. Praesent nec eros fermentum, malesuada augue a, malesuada quam. Sed bibendum libero eget metus faucibus, nec molestie metus lobortis. Nulla in ultrices leo. Sed sodales turpis augue, eget molestie arcu malesuada ut. In faucibus pellentesque nibh vitae varius. Integer et tellus eu velit fermentum tincidunt. Sed nec dui enim. In eu dui a quam bibendum vehicula. Aenean semper enim quis elit vehicula, at consequat neque efficitur. Donec feugiat malesuada elit, sed aliquet elit.
-
-  Praesent in mauris quis sapien dictum pretium. Integer rutrum ultrices tincidunt. Mauris nec mauris quam. Etiam in arcu vitae quam rhoncus gravida at eu enim. Praesent varius, turpis eget consectetur viverra, nibh mi hendrerit velit, in faucibus nisl lacus at ante. Sed vestibulum laoreet arcu quis bibendum. Duis tincidunt, lacus in luctus dignissim, purus sapien sollicitudin tortor, id dignissim neque ex non lacus. Suspendisse lobortis hendrerit turpis at suscipit. Nam euismod, mi vel egestas pretium, justo quam lobortis nunc, eu bibendum purus eros at ipsum. Etiam eget massa id arcu laoreet rhoncus. Vestibulum efficitur bibendum diam, id iaculis ante rutrum eu. Aliquam erat volutpat. Nam suscipit consequat tellus, ut varius nisl blandit sit amet. Sed tempor turpis at magna bibendum, in pharetra metus lacinia. Curabitur ut turpis diam. Nulla sit amet est in ante consequat consequat vitae et arcu.</p>
-
-          <div className='article_social'>Udostępnij</div>
-          </div>
-        </article>
+          {error && <p>Error</p>}
+          {loading && <p>Loading</p>}
+          <article className='article_container'>
+            <div className="img_container">
+              <img src={article?.main_image} alt="Smart Home" />
+            </div>
+            <div className='article_content'>
+              <Title title={article?.title ? article.title : ''}></Title>
+              <div className='article_data'>
+                <span>{formattedDate}</span>
+                </div>
+              <p>{ article?.content}</p>
+            <div className='article_social'>Udostępnij</div>
+            </div>
+          </article>
       </div>
     </div>
   )
--- src/components/views/Blog/Blog.tsx
+++ src/components/views/Blog/Blog.tsx
@@ -8,18 +8,23 @@ import './Blog.scss'
 import { getArticles } from '../../../config'
 import { secondsToDate } from '../../../utils/utils'
 
-interface articleType {
+interface ArticleType {
   title: string
   content: string
   main_image: string
-  published: any
+  published: {
+    seconds: number
+  }
 }
 
 const Blog = (): JSX.Element => {
-  const [articles, setArticles] = useState<articleType[]>([])
+  const [articles, setArticles] = useState<ArticleType[]>([])
   const [currentPage, setCurrentPage] = useState(1)
   const [articlesPerPage] = useState(3)
-  const [sliced, setSliced] = useState<articleType[]>([])
+  const [sliced, setSliced] = useState<ArticleType[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -31,38 +36,43 @@ const Blog = (): JSX.Element => {
         if (data) {
           setArticles(data)
         }
+        setLoading(false)
+        setError(false)
       } catch (error) {
-        console.log(error)
+        console.error(error)
+        setError(true)
       }
     }
+
     fetchArticles()
   }, [])
 
   useEffect(() => {
-    const indexOfLastTrip = currentPage * articlesPerPage
-    const indexOfFirstTrip = indexOfLastTrip - articlesPerPage
-    const currentArticles = articles.slice(indexOfFirstTrip, indexOfLastTrip)
+    const indexOfLastArticle = currentPage * articlesPerPage
+    const indexOfFirstArticle = indexOfLastArticle - articlesPerPage
+    const currentArticles = articles.slice(indexOfFirstArticle, indexOfLastArticle)
     setSliced(currentArticles)
   }, [currentPage, articles])
 
   return (
-    <div className='blog_all_container'>
-        <NavBar />
+    <div className="blog_all_container">
+      <NavBar />
       <div className="blog_all_overlay">
-      <Title title='Poznaj nas lepiej'/>
+        <Title title="Poznaj nas lepiej" />
         <div className="blog_all_inner">
-          {sliced.map((article: articleType, index: number) => {
+          {error && <p>Error</p>}
+          {loading && <p>Loading</p>}
+          {sliced.map((article: ArticleType, index: number) => {
             const date = secondsToDate(article.published.seconds).toLocaleDateString()
             return (
-            <article key={index} className='blog_all_article'>
-              <h2>{article.title}</h2>
-              <p>{date}</p>
-                <img src={article.main_image} alt="" style={{ height: '50px', width: '50px' }}/>
+              <article key={index} className="blog_all_article">
+                <h2>{article.title}</h2>
+                <p>{date}</p>
                 <p>{article.content.substring(0, 200)}...</p>
-              <Link to={`/blog/${index}`}>
-              <Button title='czytaj wiecej' color='light' />
-            </Link>
-          </article>
+                <Link to={`/blog/${index}`}>
+                  <Button title="czytaj wiecej" color="light" />
+                </Link>
+              </article>
             )
           })}
         </div>
@@ -70,7 +80,8 @@ const Blog = (): JSX.Element => {
           postsPerPage={articlesPerPage}
           totalPosts={articles.length}
           setCurrentPage={setCurrentPage}
-          currentPage={currentPage}/>
+          currentPage={currentPage}
+        />
       </div>
     </div>
   )
