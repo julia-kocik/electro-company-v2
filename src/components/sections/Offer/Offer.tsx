504a3f7896d6a46fa27d190b0042f5740536e09b
juliakocik <julia.kocik02@gmail.com>
2023-03-25 11:27:23 +0100

    Add cheks to Offer component

--- src/components/sections/Offer/Offer.tsx
+++ src/components/sections/Offer/Offer.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
+import { AiOutlineCheckSquare } from 'react-icons/ai'
 import './Offer.scss'
+
+const offerArray = [
+  'Instalacje elektryczne',
+  'Inteligentne domy',
+  'Systemy alarmowe',
+  'Systemy monitoringu',
+  'Instalacje fotowoltaiczne'
+]
 
 const Offer = (): JSX.Element => {
   const location = useLocation()
@@ -16,11 +25,9 @@ const Offer = (): JSX.Element => {
       <img src="https://images.unsplash.com/photo-1514803400321-3ca29fc47334?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80" alt="Smart Home" />
       <div className="offer_content">
         <ul>
-          <li>Instalacje elektryczne</li>
-          <li>Inteligentne domy</li>
-          <li>Systemy alarmowe</li>
-          <li>Systemy monitoringu</li>
-          <li>Instalacje fotowoltaiczne</li>
+          {offerArray.map((item, index) => (
+              <li key={index}><AiOutlineCheckSquare className='icon'/><span>{item}</span></li>
+          ))}
         </ul>
       </div>
     </div>
